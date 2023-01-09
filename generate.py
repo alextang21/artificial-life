8a5f7e1db5dc7b847235862747c243d69a43a143
Alex Tang <alexandertang2024@u.northwestern.edu>
2023-01-08 19:22:36 -0600

    part d done

--- generate.py
+++ generate.py
@@ -1,4 +1,7 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+ratio = 1
+for i in range(10):
+	pyrosim.Send_Cube(name="Box", pos=[0,0,0.5+i] , size=[ratio,ratio,ratio])
+	ratio *= 0.9
 pyrosim.End()
