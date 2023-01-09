c564318e90c4ecbca76d0ef327c127416db0c00a
Alex Tang <alexandertang2024@u.northwestern.edu>
2023-01-08 23:53:15 -0600

    part e done

--- generate.py
+++ generate.py
@@ -1,7 +1,19 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
-ratio = 1
-for i in range(10):
-	pyrosim.Send_Cube(name="Box", pos=[0,0,0.5+i] , size=[ratio,ratio,ratio])
-	ratio *= 0.9
-pyrosim.End()
+
+def Create_World():
+	pyrosim.Start_SDF("world.sdf")
+	pyrosim.Send_Cube(name="Box", pos=[2,-3,0.5] , size=[1,1,1])
+	pyrosim.End()
+
+def Create_Robot():
+	pyrosim.Start_URDF("body.urdf")
+	pyrosim.Send_Cube(name="Link_0", pos=[0,0,0.5] , size=[1,1,1])
+	pyrosim.Send_Joint(name="Link0_Link1", parent="Link_0", child="Link_1", type="revolute", position=[0.5,0,1])
+	pyrosim.Send_Cube(name="Link_1", pos=[0.5,0,0.5] , size=[1,1,1])
+	pyrosim.Send_Joint(name="Link1_Link_2", parent="Link_1", child="Link_2", type="revolute", position=[1,0,0])
+	pyrosim.Send_Cube(name="Link_2", pos=[0.5,0,-0.5] , size=[1,1,1])
+
+	pyrosim.End()
+
+Create_World()
+Create_Robot()
