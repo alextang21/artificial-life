3c57e6dd66516d40d011502ad61b45993ac1ea80
Alex Tang <alexandertang2024@u.northwestern.edu>
2023-01-09 22:00:44 -0600

    fixed orientation and number of blocks

--- generate.py
+++ generate.py
@@ -1,7 +1,8 @@
 import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("boxes.sdf")
-ratio = 1
-for i in range(10):
-	pyrosim.Send_Cube(name="Box", pos=[0,0,0.5+i] , size=[ratio,ratio,ratio])
-	ratio *= 0.9
+for j in range(10):
+	ratio = 1
+	for i in range(10):
+		pyrosim.Send_Cube(name="Box", pos=[0+j,0,0.5+i] , size=[ratio,ratio,ratio])
+		ratio *= 0.9
 pyrosim.End()
