5be5c3bfb5a7cbecba0da7bedce3084915c1b4b5
Alex Tang <alexandertang2024@u.northwestern.edu>
2023-02-20 23:20:19 -0600

    assignment 7 done

--- simulation.py
+++ simulation.py
@@ -36,7 +36,7 @@ class SIMULATION:
 			self.robot.Think()
 			self.robot.Act(i)
 			if self.directOrGUI == "GUI":
-				time.sleep(1/300)
+				time.sleep(1/60)
 				pass
 
 	def Get_Fitness(self):
--- solution.py
+++ solution.py
@@ -9,14 +9,19 @@ import constants as c
 
 class SOLUTION:
 	def __init__(self, myID):
-		self.numSensorNeurons = random.randint(3,10)
-		# self.numSensorNeurons = 3
-		if self.numSensorNeurons % 2 == 0:
-			self.numSensorNeurons += 1
-		self.numMotorNeurons = self.numSensorNeurons + 1
-		self.weights = numpy.random.rand(self.numSensorNeurons,self.numMotorNeurons)
+		self.limbs = {}
+		self.num_limbs = [i for i in range(random.randint(2,5)*2+1)]
+		self.sensors = numpy.random.randint(2, size = len(self.num_limbs))
+		self.motors = numpy.random.randint(2, size = len(self.num_limbs))
+		self.colors = {0: ["Blue", [0,0,1.0,1.0]], 1: ["Green", [0,1.0,0,1.0]]}
+		self.motors[0] = 0
+		
+		self.weights = numpy.random.rand(numpy.count_nonzero(self.sensors == 1), numpy.count_nonzero(self.motors == 1))
 		self.weights = self.weights * 2 - 1
 		self.myID = myID
+		# limbs is a dictionary
+		# limbs: key is the number ID of the limb, value is a list with [sensor?, size[], color, colorRGBA]
+		
 
 	def Start_Simulation(self, directOrGUI):
 		self.Create_World()
@@ -37,7 +42,8 @@ class SOLUTION:
 		self.Wait_For_Simulation_To_End()
 
 	def Mutate(self):
-		self.weights[random.randint(0,2)][random.randint(0,1)] = random.random() * 2 - 1
+		pass
+		# self.weights[random.randint(0,numpy.count_nonzero(self.sensors == 1)-1)][random.randint(0,numpy.count_nonzero(self.motors == 1)-1)] = random.random() * 2 - 1
 
 	def Set_ID(self, ID):
 		self.myID = ID
@@ -48,66 +54,53 @@ class SOLUTION:
 		pyrosim.End()
 
 	def Generate_Body(self):
-		# limbs is a dictionary
-		num_limbs = [i for i in range(random.randint(1,10))]
-		sensors = numpy.random.randint(2, size = len(num_limbs))
-		colors = {0: ["Blue", [0,0,1.0,1.0]], 1: ["Green", [0,1.0,0,1.0]]}
-		# limbs: key is the number ID of the limb, value is a list with [sensor?, size[], color, colorRGBA]
-		limbs = {0: [sensors[0], numpy.random.rand(3) * random.randint(1,2) + 1, colors[sensors[0]][0], colors[sensors[0]][1]]}
+
+		for i in range(len(self.num_limbs)):
+			self.limbs[i] = [self.sensors[i], self.motors[i], numpy.random.rand(3) * random.randint(1,2)/2 + 1, self.colors[self.sensors[i]][0], self.colors[self.sensors[i]][1]]
+		start_z = 0.0
+		for i in self.limbs:
+			start_z += self.limbs[i][2][2]
+
+		start_z /= 2
+		
 		pyrosim.Start_URDF(f"body{self.myID}.urdf")
-		# sizes = limbs[id][1]
-		pyrosim.Send_Cube(name="FrontLeg0", pos=limbs[0][1]/2 , size=limbs[0][1], color=limbs[0][2], colorRGBA=limbs[0][3])
-		pyrosim.Send_Joint(name="FrontLeg0_FrontLeg1", parent="FrontLeg0", child="FrontLeg1",  type="revolute", position=[sizes[0]/2,sizes[1]/2,0], jointAxis = "0 0 1")
-		pyrosim.Send_Cube(name="FrontLeg1", pos=limbs[1][1]/2 , size=limbs[1][1], color=limbs[1][2], colorRGBA=limbs[1][3])
-		pyrosim.Send_Joint(name="FrontLeg1_FrontLeg2", parent="FrontLeg1", child="FrontLeg2",  type="revolute", position=[sizes[0]/2,sizes[1]/2,0], jointAxis = "0 0 1")
-		pyrosim.Send_Cube(name="FrontLeg2", pos=limbs[2][1]/2 , size=limbs[2][1], color=limbs[2][2], colorRGBA=limbs[2][3])
-		pyrosim.Send_Joint(name="FrontLeg2_FrontLeg3", parent="FrontLeg2", child="FrontLeg3",  type="revolute", position=[sizes[0]/2,sizes[1]/2,0], jointAxis = "0 0 1")
-		pyrosim.Send_Cube(name="FrontLeg1", pos=sizes/2 , size=sizes, color="Green", colorRGBA=[0,1.0,0,1.0])
-		pyrosim.Send_Joint(name="Torso_FrontLeg1", parent="Torso", child="FrontLeg1", type="revolute", position=[sizes[0]/2,sizes[1]/2,0], jointAxis = "0 0 1")
-		pyrosim.Send_Cube(name="FrontLeg1", pos=sizes/2 , size=sizes, color="Green", colorRGBA=[0,1.0,0,1.0])
-		for i in range(0, self.numMotorNeurons-1):
-			sensor = random.randint(0,7)
-			sizes = numpy.random.rand(3) * random.randint(1,2) + 1
-			if sensor < 4:
-				pyrosim.Send_Cube(name=f"FrontLeg{i+1}", pos=sizes/2 , size=sizes, color="Blue", colorRGBA=[0,0,1.0,1.0])
-				pyrosim.Send_Joint(name=f"FrontLeg{i}_FrontLeg{i+1}", parent=f"FrontLeg{i}", child=f"FrontLeg{i+1}", type="revolute", position=[sizes[0]/2,sizes[1]/2,0], jointAxis = "0 0 1")
+		pyrosim.Send_Cube(name=f"Segment0", pos=[0,0,start_z], size=self.limbs[0][2], color=self.limbs[0][3], colorRGBA=self.limbs[0][4])
+		pyrosim.Send_Joint(name=f"Segment0_Segment1", parent=f"Segment0", child=f"Segment1",  type="revolute", position=[-self.limbs[0][2][0]/2,self.limbs[0][2][1]/2,start_z-(self.limbs[0][2][2]/4)], jointAxis = "0 1 1")
+		pyrosim.Send_Joint(name=f"Segment0_Segment2", parent=f"Segment0", child=f"Segment2",  type="revolute", position=[-self.limbs[0][2][0]/2,-self.limbs[0][2][1]/2,start_z-(self.limbs[0][2][2]/4)], jointAxis = "0 1 1")
+		for i in range(1,len(self.num_limbs)-2):
+			directions = numpy.random.randint(2, size = 3)
+			directions[2] = 1
+			if i % 2 == 0:
+				pyrosim.Send_Cube(name=f"Segment{i}", pos=[self.limbs[i][2][0]/2,-self.limbs[i][2][1]/2,-self.limbs[i][2][2]/4], size=self.limbs[i][2], color=self.limbs[i][3], colorRGBA=self.limbs[i][4])
+				pyrosim.Send_Joint(name=f"Segment{i}_Segment{i+2}", parent=f"Segment{i}", child=f"Segment{i+2}",  type="revolute", position=[self.limbs[i][2][1]/2,-self.limbs[i][2][1]/2,-self.limbs[i][2][2]] * directions, jointAxis = "0 1 1")
 			else:
-				pyrosim.Send_Cube(name=f"FrontLeg{i+1}", pos=sizes/2 , size=sizes, color="Green", colorRGBA=[0,1.0,0,1.0])
-				pyrosim.Send_Joint(name=f"FrontLeg{i}_FrontLeg{i+1}", parent=f"FrontLeg{i}", child=f"FrontLeg{i+1}", type="revolute", position=[sizes[0]/2,sizes[0]/2,0], jointAxis = "0 0 1")
-				
-				
-		# pyrosim.Send_Joint(name="FrontLeg1_FrontLeg2", parent="FrontLeg1", child="FrontLeg2", type="revolute", position=[1,0,0], jointAxis = "0 0 1")
-		# pyrosim.Send_Cube(name="FrontLeg2", pos=[0.5,0,0] , size=[1,0.5,0.5], color="Black", colorRGBA=[0,0,0,1.0])
-		# pyrosim.Send_Joint(name="FrontLeg2_FrontLeg3", parent="FrontLeg2", child="FrontLeg3", type="revolute", position=[1,0,0], jointAxis = "0 0 1")
-		# pyrosim.Send_Cube(name="FrontLeg3", pos=[0.5,0,0] , size=[1,0.5,0.5], color="Cyan", colorRGBA=[0,1.0,1.0,1.0])
+				pyrosim.Send_Cube(name=f"Segment{i}", pos=[self.limbs[i][2][0]/2,self.limbs[i][2][1]/2,-self.limbs[i][2][2]/4], size=self.limbs[i][2], color=self.limbs[i][3], colorRGBA=self.limbs[i][4])
+				pyrosim.Send_Joint(name=f"Segment{i}_Segment{i+2}", parent=f"Segment{i}", child=f"Segment{i+2}",  type="revolute", position=[self.limbs[i][2][1]/2,self.limbs[i][2][1]/2,-self.limbs[i][2][2]] * directions, jointAxis = "0 1 1")
+		pyrosim.Send_Cube(name=f"Segment{self.num_limbs[-2]}", pos=[self.limbs[self.num_limbs[-2]][2][0]/2,self.limbs[self.num_limbs[-2]][2][1]/2,-self.limbs[self.num_limbs[-2]][2][2]/4], size=self.limbs[self.num_limbs[-2]][2], color=self.limbs[self.num_limbs[-2]][3], colorRGBA=self.limbs[self.num_limbs[-2]][4])
+		pyrosim.Send_Cube(name=f"Segment{self.num_limbs[-1]}", pos=[self.limbs[self.num_limbs[-1]][2][0]/2,-self.limbs[self.num_limbs[-1]][2][1]/2,-self.limbs[self.num_limbs[-1]][2][2]/4], size=self.limbs[self.num_limbs[-1]][2], color=self.limbs[self.num_limbs[-1]][3], colorRGBA=self.limbs[self.num_limbs[-1]][4])
 
 		pyrosim.End()
 
 	def Generate_Brain(self):
 		pyrosim.Start_NeuralNetwork("brain" + str(self.myID) + ".nndf")
-		pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "BackLeg1")
-		pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "FrontLeg1")
-		pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg3")
-		sensorName = 3
-		if self.numSensorNeurons > 3:
-			for i in range(3,self.numSensorNeurons):
-				if i % 2 == 0:
-					pyrosim.Send_Sensor_Neuron(name = sensorName , linkName = f"FrontLeg{i+1}")
-					sensorName += 1
+		neuron_counter = 0
+		for i in self.limbs:
+			if self.limbs[i][0] == 1:
+				pyrosim.Send_Sensor_Neuron(name = neuron_counter, linkName = f"Segment{i}")
+				neuron_counter += 1
 		
-		pyrosim.Send_Motor_Neuron( name = sensorName, jointName = "Torso_BackLeg1")
-		pyrosim.Send_Motor_Neuron( name = sensorName + 1 , jointName = "Torso_FrontLeg1")
-		pyrosim.Send_Motor_Neuron( name = sensorName + 2 , jointName = "FrontLeg1_FrontLeg2")
-		pyrosim.Send_Motor_Neuron( name = sensorName + 3 , jointName = "FrontLeg2_FrontLeg3")
-		motorName = numpy.array([3,4])
-		if self.numMotorNeurons > 4:
-			for i in range(4, self.numMotorNeurons):
-				pyrosim.Send_Motor_Neuron( name = sensorName + i , jointName = f"FrontLeg{motorName[0]}_FrontLeg{motorName[1]}")
-				motorName = motorName + 1
+		for i in self.limbs:
+			if self.limbs[i][1] == 1:
+				if i == 1:
+					pyrosim.Send_Motor_Neuron( name = neuron_counter, jointName = f"Segment{i-1}_Segment{i}")
+					neuron_counter += 1
+				else:
+					pyrosim.Send_Motor_Neuron( name = neuron_counter, jointName = f"Segment{i-2}_Segment{i}")
+					neuron_counter += 1
 
-		for currentRow in range(self.numSensorNeurons):
-			for currentColumn in range(self.numMotorNeurons):
-				pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn + sensorName , weight = self.weights[currentRow][currentColumn] )
+		for currentRow in range(numpy.count_nonzero(self.sensors == 1)):
+			for currentColumn in range(numpy.count_nonzero(self.motors == 1)):
+				pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn + numpy.count_nonzero(self.sensors == 1) , weight = self.weights[currentRow][currentColumn] )
 
 		pyrosim.End()
 
